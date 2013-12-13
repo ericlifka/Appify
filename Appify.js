5e020a21cff1d3d6bbeed91de343f5c05d5bfa34
Eric Lifka <eric.lifka@gmail.com>
2013-12-12 22:28:31 -0500

    Route and Renderer structure working, templating next

--- Appify.js
+++ Appify.js
@@ -5,14 +5,90 @@
 		throw "jQuery is required for Appify to work";
 	}
 
+	var isMiddlewareCall = function (args) {
+		return args.length === 1 && 'function' === typeof args[0];
+	};
+
 	var Appify = function (appCallback) { 
 		var appStart = function () {
+			var routes = {};
+			var routeMiddleware = [];
+
+			var renderers = {};
+			var rendererMiddleware = [];
+
+			var route = function (name, callback) {
+				if (isMiddlewareCall(arguments)) {
+					routeMiddleware.push(arguments[0]);
+				} else if (arguments.length === 2) {
+					routes[name] = callback;
+				} else {
+					throw 'Unrecognized call signature for app.route';
+				}
+			};
+
+			var renderer = function (name, callback) {
+				if (isMiddlewareCall(arguments)) {
+					rendererMiddleware.push(arguments[0]);
+				} else if (arguments.length === 2) {
+					renderers[name] = callback;
+				} else {
+					throw 'Unrecognized call signature for app.renderer';
+				}
+			};
+
+			var render = function (rendererName, details) {
+				var renderer = renderers[rendererName];
+				if (!renderer) {
+					throw "No handler provided for renderer: " + rendererName;
+				}
+
+				var current = 0;
+				var $outlet = function (updatedDetails) {
+					var middleware = rendererMiddleware[current];
+					current += 1;
+
+					if (middleware) {
+						return middleware($outlet, updatedDetails);
+					} else {
+						return renderer(updatedDetails);
+					}
+				};
+
+				var viewTree = $outlet(details);
+				console.log('rendering complete', viewTree);
+			};
+
+			var transitionTo = function (routeName, details) {
+				var routeHandler = routes[routeName];
+				if (!routeHandler) {
+					throw "No handler provided for route: " + routeName;
+				}
+
+				var current = 0;
+				var next = function (updatedDetails) {
+					var middleware = routeMiddleware[current];
+					current += 1;
+
+					if (middleware) {
+						middleware(next, updatedDetails);
+					} else {
+						routeHandler(render, updatedDetails);
+					}
+				};
+
+				next(details);
+			};
+
 			var app = {
-				route: function () {},
-				renderer: function () {}
+				route: route,
+				renderer: renderer,
+				transitionTo: transitionTo
 			};
 
 			appCallback(app);
+
+			app.transitionTo('index');
 		};
 
 		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
@@ -23,8 +99,12 @@
 	};
 
 	var elementShill = {
-		'on': function () {},
-		'class': function () {}
+		'on': function () {
+			return this;
+		},
+		'class': function () {
+			return this;
+		}
 	};
 
 	Appify.templates = {
