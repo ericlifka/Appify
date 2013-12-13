5342dc79c0c4ac186e5edc9b35306797135a21bc
Eric Lifka <eric.lifka@gmail.com>
2013-12-12 22:30:16 -0500

    clears routing log statements from simple sample

--- samples/simple_sample.js
+++ samples/simple_sample.js
@@ -3,34 +3,24 @@ Appify(function (app) {
 	var $a = Appify.templates.$a;
 
 	app.route(function (next, details) {
-		console.log('route middleware');
-		
 		next(details);
 	});
 
 	app.route('index', function (render, details) {
-		console.log('route index');
-		
 		render('index', details);
 	});
 
 	app.route('other', function (render, details) {
-		console.log('route other');
-
 		render('other', details);
 	});
 
 	app.renderer(function ($outlet, details) {
-		console.log('renderer middleware');
-
 		return $div(
 			$outlet(details)
 		).class('application');
 	});
 
 	app.renderer('index', function (details) {
-		console.log('renderer index');
-
 		return $div(
 			$div('Home Page'),
 			$a('Go to Other Page').on('click', function () {
@@ -40,8 +30,6 @@ Appify(function (app) {
 	});
 
 	app.renderer('other', function (details) {
-		console.log('renderer other');
-
 		return $div(
 			$div('Other Page'),
 			$a('Go to Home Page').on('click', function () {
