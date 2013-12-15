e9b38ef61a50874d3bb16c1e020d43acae87ebec
Eric Lifka <eric.lifka@gmail.com>
2013-12-14 20:46:40 -0500

    Templating behavior, needs more tags and toString behavior

--- Appify.js
+++ Appify.js
@@ -56,6 +56,7 @@
 				};
 
 				var viewTree = $outlet(details);
+				debugger;
 				console.log('rendering complete', viewTree);
 			};
 
@@ -92,24 +93,101 @@
 		}
 	};
 
-	var elementShill = {
-		'on': function () {
-			return this;
-		},
-		'class': function () {
-			return this;
+	// Templating
+	var HtmlElement = function () {
+		if (window === this) {
+			throw "HtmlElement constructor called without 'new' keyword";
+		}
+
+		this.children = [];
+		this.attributes = {};
+		this.events = {};
+	};
+	HtmlElement.prototype.on = function (event, handler) {
+		if (!this.events[event]) {
+			this.events[event] = [];
+		}
+		this.events[event].push(handler);
+		return this;
+	};
+	HtmlElement.prototype.class = function (/* classNames... */) {
+		for (var i = 0, length = arguments.length; i < length; i++) {
+			this.attr('class', arguments[i]);
+		}
+		return this;
+	};
+	HtmlElement.prototype.attr = function (attr, value) {
+		if (!this.attributes[attr]) {
+			this.attributes[attr] = value;
+		}
+		else if ($.isArray(this.attributes[attr])) {
+			this.attributes[attr].push(value);
+		} else {
+			var original = this.attributes[attr];
+			this.attributes[attr] = [original, value];
+		}
+		return this;
+	};
+	HtmlElement.prototype.tag = function (tag) {
+		this.tag = tag;
+		return this;
+	};
+	HtmlElement.prototype.child = function (child) {
+		this.children.push(child);
+		return this;
+	};
+	HtmlElement.prototype.children = function (children) {
+		for (var i = 0, length = children.length; i < length; i++) {
+			this.children.push(children[i]);
+		}
+		return this;
+	};
+
+	var isHtmlElement = function (elem) {
+		return elem instanceof HtmlElement;
+	};
+
+	var createElem = function (tag, args) {
+		var elem = new HtmlElement();
+		elem.tag(tag);
+
+		for (var i = 0, length = args.length; i < length; i++) {
+			var arg = args[i];
+			if (isHtmlElement(arg) || 'string' === typeof arg) {
+				elem.child(arg);
+			} else {
+				for (var attr in arg) {
+					if (arg.hasOwnProperty(attr)) {
+						elem.attr(attr, arg[attr]);
+					}
+				}
+			}
+		}
+		return elem;
+	};
+
+	var generateTagFunction = function (tag) {
+		return function () {
+			return createElem(tag, arguments);
+		};
+	};
+
+	var templates = {
+		HtmlElement: HtmlElement,
+		generateTagFunction: generateTagFunction,
+		$event: function (elem, event, handler) {
+			return elem.on(event, handler);
 		}
 	};
 
-	Appify.templates = {
-		$div: function() {
-			return elementShill;
-		},
-		$a: function () {
-			return elementShill;
-		}
+	var tags = ['div', 'a'];
+	var length = tags.length;
+	for (var i = 0; i < length; i++) {
+		(function (tag) {
+			templates['$'+tag] = generateTagFunction(tag);
+		})(tags[i]);
 	}
 
+	Appify.templates = templates;
 	window.Appify = Appify;
-
 }());
